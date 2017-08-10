dfc708d32ad89fd2a795ef0ae1b8cc7f08d8c053
CARLOS RAUL PEREZ MORENO <caul58@gmail.com>
2017-08-10 07:31:50 +0200

    feat(): detail view controller with an Hero as data source to configure the UI

--- MarvelHeroes/DetailViewController.swift
+++ MarvelHeroes/DetailViewController.swift
@@ -7,17 +7,22 @@
 //
 
 import UIKit
+import Kingfisher
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
+    @IBOutlet weak var infoTextView: UITextView!
+    @IBOutlet weak var photoImage: UIImageView!
 
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
+            title = detail.name
+            if let textView = infoTextView {
+                textView.text = "Real Name: \(detail.aka) \nHeight: \(detail.height)\nPower: \n\(detail.power)\nAbilities:\n\(detail.abilities)\nGroups: \n\(detail.groups)"
+            }
+            if let imageView = photoImage {
+                imageView.kf.setImage(with: detail.photoURL, placeholder: UIImage(named: "blank-avatar"))
             }
         }
     }
@@ -33,13 +38,16 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    var detailItem: NSDate? {
+    var detailItem: Hero! {
         didSet {
             // Update the view.
             configureView()
         }
     }
 
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool{
+        return false
+    }
 
 }
 
